c1df96398176beba4ba26492555973727e902cc1
gvp-raviteja <ganireddy.raviteja@gmail.com>
2017-03-28 12:21:20 -0400

    increased action space and stacked multiple frames

--- carRacing.py
+++ carRacing.py
@@ -1,66 +1,38 @@
-# -*- coding: utf-8 -*-
 import copy
 import gym
 import numpy as np
+import random
 from collections import deque
+import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, LSTM, Reshape
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.optimizers import RMSprop
 
-episodes = 3000 #1000
-episode_length = 250
-render_after = 1500
+episodes = 100000 #1000
+episode_length = 1000
+render_after = 3#7000
 
-def format_action_rand(a):
-    #Convert random actions to compatible values
-    #Steer, Gas, Brake    
-    
-    if -1 <= a[0] < -0.5:
-        return(np.array([-1, 0.2, 0]))
-    if -0.5 <= a[0] <= 0.5 :
-        return(np.array([0, 0.2, 0]))    
-    if +1 >= a[0] > 0.5:
-        return(np.array([+1, 0.2, 0]))
-    
-    #Else
-    print("Error, a is " + str(a))
+car_action_space = {
+    0 : [0,0.5,0],
+    1 : [-0.5,0.5,0],
+    2 : [0.5,0.5,0],
+    3 : [-1,0.5,0],
+    4 : [1,0.5,0],
+    5 : [-0.5,0.5,0.2],
+    6: [0.5,0.5,0.2],
+    7: [-1,0.5,0.2],
+    8: [1,0.5,0.2]
+}
+num_actions = 9
 
-
-def format_action_pred(a):
-    #Convert predicted actions to compatible values
-    #Steer, Gas, Brake
-    
-    if a == 0:
-        return(np.array([-1, 0.2, 0]))
-    if a == 1:
-        return(np.array([0, 0.2, 0]))    
-    if a == 2:
-        return(np.array([+1, 0.2, 0]))
-    #Else
-    print("Error, a is " + str(a))
-
-def convert_action(a):
-    #Convert stored actions to compatible values
-    #Steer, Gas, Brake    
-    
-    if action[0] == -1:
-        return(0)
-    if action[0] ==  0:
-        return(1)    
-    if action[0] == +1:
-        return(2)
-    
-    #Else
-    print("Error, a is " + str(action))    
-    
-
-
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 class DQNAgent:
-    def __init__(self, env):
-        self.env = env
-        self.memory = deque(maxlen=5000) #10000
+    def __init__(self):
+        self.num_actions = num_actions
+        self.memory = deque(maxlen=100000) #10000
         self.gamma = 0.9  # decay rate
         self.epsilon = 1.0  # exploration
         self.epsilon_decay = .99
@@ -71,38 +43,37 @@ class DQNAgent:
     def _build_model(self):
         # Deep-Q learning Model
         model = Sequential()
-        #model.add(Dense(64, input_dim=4, activation='tanh', init='he_uniform'))
-        model.add(Convolution2D(64, 8, 8, border_mode='same',
-                        input_shape=(96,96,3))
-                 )
+        # model.add(Dense(64, input_dim=4, activation='tanh', init='he_uniform'))
+        model.add(Convolution2D(64, 4, 4, border_mode='same',
+                                input_shape=(96, 96, 3))
+                  )
         model.add(Activation('relu'))
-        model.add(Convolution2D(64, 4, 4))
+        model.add(Convolution2D(128, 4, 4))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
+        # model.add(MaxPooling2D(pool_size=(2, 2))) #No Max Pool as this we want to see translation
         model.add(Dropout(0.25))
         model.add(Convolution2D(128, 4, 4))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(4, 4)))
+        # model.add(MaxPooling2D(pool_size=(4, 4)))
         model.add(Dropout(0.25))
         model.add(Convolution2D(128, 4, 4))
         model.add(Activation('relu'))
-        model.add(MaxPooling2D(pool_size=(4, 4)))
+        # model.add(MaxPooling2D(pool_size=(4, 4)))
         model.add(Dropout(0.25))
 
-        
-        #model.add(Reshape( (5, 8) ))
-        #model.add(LSTM(8, return_sequences=True))
-        
-        #model.add(Dense(64, input_dim=96*96*3, activation='tanh', init='he_uniform'))
-        #model.add(Dense(128, activation='tanh', init='he_uniform'))
-        #model.add(Dense(128, activation='tanh', init='he_uniform'))
-        
+        # model.add(Reshape( (5, 8) ))
+        # model.add(LSTM(8, return_sequences=True))
+
+        # model.add(Dense(64, input_dim=96*96*3, activation='tanh', init='he_uniform'))
+        # model.add(Dense(128, activation='tanh', init='he_uniform'))
+        # model.add(Dense(128, activation='tanh', init='he_uniform'))
+
 
         model.add(Flatten())
-        model.add(Dense(256, activation='tanh', init='he_uniform'))
         model.add(Dense(128, activation='tanh', init='he_uniform'))
-        model.add(Dense(32, activation='tanh', init='he_uniform'))
-        model.add(Dense(3, activation='linear', init='he_uniform'))
+        model.add(Dense(128, activation='tanh', init='he_uniform'))
+        # model.add(Dense(32, activation='tanh', init='he_uniform'))
+        model.add(Dense(self.num_actions, activation='linear', init='he_uniform'))
         model.compile(loss='mse',
                       optimizer=RMSprop(lr=self.learning_rate))
         self.model = model
@@ -113,13 +84,12 @@ class DQNAgent:
     def act(self, state): #Reform returned actions
         #Random Action
         if np.random.rand() <= self.epsilon:
-            a = self.env.action_space.sample()
-            return(format_action_rand(a))
-        
+            return  random.randrange(self.num_actions)
+
         #Else predicted action
         act_values = self.model.predict(state)
-        a = np.argmax(act_values[0])
-        return(format_action_pred(a))   # returns action
+        return np.argmax(act_values[0])
+           # returns action
 
     def replay(self, batch_size):
         batchs = min(batch_size, len(self.memory))
@@ -131,7 +101,7 @@ class DQNAgent:
                 target = reward + self.gamma * \
                             np.amax(self.model.predict(next_state)[0])
             target_f = self.model.predict(state)
-            action_c = convert_action(action)
+            action_c = action
             target_f[0][action_c] = target
             self.model.fit(state, target_f, nb_epoch=1, verbose=0)
             
@@ -145,43 +115,55 @@ class DQNAgent:
         self.model.save_weights(name)
 
 if __name__ == "__main__":
-    #env = gym.make('CartPole-v0')
     env = gym.make('CarRacing-v0')
-    agent = DQNAgent(env)
+    agent = DQNAgent()
     print(agent.model.summary())
     print("Agent made...")
-    # agent.load("./save/cartpole-starter.h5")
-
+    #agent.load("/home/exx/gatordriving/carracing_Thu-Fri.h5")
     for e in range(episodes):
+        state_queue = deque(maxlen=4)
         total_reward = 0
-        print("Episode " + str(e))
+        # print("Episode " + str(e))
         state = env.reset()
+        # print state.shape
         #state = np.reshape(state, [1, 4])
-        state = np.reshape(state, [1, 96, 96, 3])
+        state = np.dot(state[...,:3], [0.299, 0.587, 0.114])
+        state = np.reshape(state, [1, 96, 96])
+        for i in range(3):
+            state_queue.append(state)
+        # print state_queue
         for time_t in range(episode_length): #5000
-            if e > render_after:
+            if e%100==0:
                 env.render()
-            #print("Time step " + str(time_t) )
-            action = agent.act(state)
+            # print("Time step " + str(time_t) )
+            frame =  np.reshape(np.stack(state_queue, axis=-1),[1, 96, 96, 3])
+            action = agent.act(frame)
             #print(action)
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, _ = env.step(car_action_space[action])
             #next_state = np.reshape(next_state, [1, 4])
-            next_state = np.reshape(next_state, [1, 96, 96, 3])
-            
+            next_state = rgb2gray(next_state)
+            next_state = np.reshape(next_state, [1, 96, 96])
+            old_state = copy.deepcopy(frame)
+            state_queue.append(next_state)
+            state_queue.popleft()
             total_reward = total_reward + reward
             #reward = -100 if done else reward
-            agent.remember(state, action, reward, next_state, done)
-            state = copy.deepcopy(next_state)
+            frame = np.reshape(np.stack(state_queue, axis=-1), [1, 96, 96, 3])
+            # print done
+            agent.remember(old_state, action, reward, frame, done)
             if done:
                 print("episode: {}/{}, score: {}, memory size: {}, e: {}"
                       .format(e, episodes, time_t,
                               len(agent.memory), agent.epsilon))
                 break
-                
-        print(total_reward)
-                           
+
+        # print(total_reward)
+
+
+
         if e % 50 == 0:
-            agent.save("./save/carracing.h5")
-        agent.replay(100)
-        
+            agent.save("/home/exx/gatordriving/carracing.h5")
+        agent.replay(1000)
+
     print("All episodes over")
+
